84f75d8c3623cb0aea9e9aaf8b10fbd7c190821b
tts <tts@ymsht.info>
2019-09-14 21:55:10 +0900

    必要なカラムのみ取得

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -416,16 +416,18 @@ func queryChannels() ([]int64, error) {
 }
 
 func queryHaveRead(userID, chID int64) (int64, error) {
-	type HaveRead struct {
-		UserID    int64     `db:"user_id"`
-		ChannelID int64     `db:"channel_id"`
-		MessageID int64     `db:"message_id"`
-		UpdatedAt time.Time `db:"updated_at"`
-		CreatedAt time.Time `db:"created_at"`
-	}
-	h := HaveRead{}
+	//type HaveRead struct {
+	//	UserID    int64     `db:"user_id"`
+	//	ChannelID int64     `db:"channel_id"`
+	//	MessageID int64     `db:"message_id"`
+	//	UpdatedAt time.Time `db:"updated_at"`
+	//	CreatedAt time.Time `db:"created_at"`
+	//}
+	//h := HaveRead{}
 
-	err := db.Get(&h, "SELECT * FROM haveread WHERE user_id = ? AND channel_id = ?",
+	var messageId int64
+
+	err := db.Get(&messageId, "SELECT message_id FROM haveread WHERE user_id = ? AND channel_id = ?",
 		userID, chID)
 
 	if err == sql.ErrNoRows {
@@ -433,7 +435,8 @@ func queryHaveRead(userID, chID int64) (int64, error) {
 	} else if err != nil {
 		return 0, err
 	}
-	return h.MessageID, nil
+	//return h.MessageID, nil
+	return messageId, nil
 }
 
 func fetchUnread(c echo.Context) error {
