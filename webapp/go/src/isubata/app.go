92960f44b1623d4cdafc6e2e32a8aacd027c7f25
tts <tts@ymsht.info>
2019-09-15 00:59:00 +0900

    コネクション数増加

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -78,7 +78,7 @@ func init() {
 		time.Sleep(time.Second * 3)
 	}
 
-	db.SetMaxOpenConns(12000)
+	db.SetMaxOpenConns(2400)
 	db.SetConnMaxLifetime(5 * time.Minute)
 	log.Printf("Succeeded to connect db.")
 }
